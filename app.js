f4ff9e53ab6fc00fbc89b34112442646a9ddcd2a
FabioZirondi <fabio96zirondi@gmail.com>
2024-11-03 21:15:12 -0300

    Fiz o pesquisar, concertei a navbar

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'routes/views/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => { 
+    console.log(`Servidor rodando em http://0.0.0.0:${PORT}`);
+});
+
+/* const express = require('express');
+const path = require('path');
+const app = express();
+
 // Middleware para servir arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -13,5 +29,5 @@ app.get('/', (req, res) => {
 // Porta do servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+    console.log(Servidor rodando em http://localhost:${PORT});
+});*/
